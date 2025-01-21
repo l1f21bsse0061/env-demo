b555d2a1720d5301220fce28be08e6b8d603c748
l1f21bsse0061 <l1f21bsse0061@ucp.edu.pk>
2025-01-21 17:36:56 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to handle the button click
+document.getElementById("contactButton").addEventListener("click", function() {
+    alert("Thank you for reaching out! We'll get back to you soon.");
+});
